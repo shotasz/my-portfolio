f6fdb5a28e3d74952b7152b09aec78d1faaaa3c3
shotasz <s2shota123gmail.com>
2021-07-09 23:01:18 +0900

    siderightの構築

---
+++ components/sideright.js
@@ -0,0 +1,10 @@
+import styles from "../styles/Sideright.module.scss";
+import Image from "next/image";
+
+export function Sideright() {
+  return (
+    <div className={styles.sidebarRight}>
+      <a>s2shota123@gmail.com</a>
+    </div>
+  );
+}
