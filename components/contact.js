8d995d9b1af4bbafb1c33151b4b12f96043492c6
shotasz <s2shota123gmail.com>
2021-07-25 09:39:36 +0900

    contactセクショんの構築

--- components/contact.js
+++ components/contact.js
@@ -3,7 +3,16 @@ import styles from "../styles/Contact.module.scss";
 export function Contact() {
   return (
     <section className={styles.contact}>
-      <h2>SECTION 4</h2>
+      <span className={styles.contact__section_start}>&lt;section&gt;</span>
+      <div className={styles.contact__content}>
+        <span className={styles.contact__h2_start}>&lt;h2&gt;</span>
+        <h2>Contact Me</h2>
+        <span className={styles.contact__h2_end}>&lt;/h2&gt;</span>
+        <span className={styles.contact__button_start}>&lt;button&gt;</span>
+        <button className={styles.btn}>SEND MAIL</button>
+        <span className={styles.contact__button_end}>&lt;/button&gt;</span>
+      </div>
+      <span className={styles.contact__section_end}>&lt;/section&gt;</span>
     </section>
   );
 }
