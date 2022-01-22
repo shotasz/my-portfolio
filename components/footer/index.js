b384e28680ec09eea787d0fd0ba6a18d2e68e940
shotasz <s2shota123@gmail.com>
2022-01-22 16:06:30 +0900

    ページ全体でスタイルを変更

--- components/footer/index.js
+++ components/footer/index.js
@@ -6,7 +6,7 @@ export function Footer() {
       <span className={styles.footer__footer_start}>&lt;footer&gt;</span>
       <div className={styles.footer__content}>
         <p>
-          Designed & Build by <a>Shota Suzuki</a>
+          Designed & Built by <a>Shota Suzuki</a>
         </p>
       </div>
       <span className={styles.footer__footer_end}>&lt;/footer&gt;</span>
