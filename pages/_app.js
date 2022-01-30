69458cf32a38ec00e0d1c6522e5d2c740f1e617a
shotasz <s2shota123@gmail.com>
2022-01-30 18:20:17 +0900

    chakraUIの導入

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
 import "../styles/_globals.scss";
 
-import { Fragment } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../components/ui/theme";
 import { Headline } from "../components/headline";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Fragment>
+    <ChakraProvider theme={theme}>
       <Headline />
       <Component {...pageProps} />
-    </Fragment>
+    </ChakraProvider>
   );
 }
 
