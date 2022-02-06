bd3b3bbab815054d5923ac89f0b28a7b525829b7
shotasz <s2shota123@gmail.com>
2022-02-06 11:02:52 +0900

    ハンバーガーリストのスタイルをコミット

--- components/navigation/navigation-list.js
+++ components/navigation/navigation-list.js
@@ -7,25 +7,36 @@ const StyleList = styled.li`
   text-transform: capitalize;
   padding: 1rem 2rem;
 
+  &:link,
+  &:visited {
+    display: inline-block;
+    font-size: 3rem;
+    font-weight: 300;
+    padding: 1rem 2rem;
+    color: var(--color-white);
+  }
+
   &::before {
     content: "0${(props) => props.id - 1}.";
     display: inline-block;
     font-family: var(--font-roboto);
     font-weight: 300;
     color: var(--color-green);
-    font-size: 1.5rem;
+    font-size: 3rem;
     margin-right: 0.7rem;
   }
 
   a {
-    color: var(--color-lightest-slate) !important;
+    color: var(--color-white);
+    font-size: 4rem;
     font-family: var(--font-ubuntu);
-    font-weight: 500;
+    font-weight: 700;
     letter-spacing: 1px;
+    text-decoration: none;
 
     &:hover,
     &:focus {
-      color: var(--color-green) !important;
+      color: var(--color-green);
     }
   }
 `;
