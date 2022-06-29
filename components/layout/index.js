e12cb4ad51761d3090346992e135776a8ec5b866
shotasz <s2shota123@gmail.com>
2022-06-29 16:30:36 +0900

    revealSectionHandlerのリファクター

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,8 +1,14 @@
 import styles from "./index.module.scss";
 import { Header } from "../header";
 import { Fragment } from "react";
+import { useEffect } from "react";
+import { revealSectionHandler } from "../ui/utilities";
 
 export function Layout(props) {
+  useEffect(() => {
+    revealSectionHandler();
+  }, []);
+
   return (
     <Fragment>
       <Header />
--- components/ui/utilities.js
+++ components/ui/utilities.js
@@ -1,30 +1,32 @@
+import styles from "./utilities.module.scss";
+
 export function revealSectionHandler() {
   const allSections = document.querySelectorAll("section");
 
   const revealSection = (entries, observer) => {
     const [entry] = entries;
 
-    if (!entry.isIntersecting) return;
-
     if (entry.target.id === "hero") return;
 
-    // entry.target.style.transform = "translateY(50px)";
-    //fadeUpAnimeグローバルに設定
-    entry.target.style.opacity = "1";
-    entry.target.style.animation = "fadeUpAnime 0.75s ease-out";
+    if (!entry.isIntersecting) return;
+
+    if (entry.isIntersecting) {
+      entry.target.classList.remove(`${styles.disactive}`);
+      entry.target.classList.add(`${styles.active}`);
+    }
 
     observer.unobserve(entry.target);
   };
 
   const sectionObserver = new IntersectionObserver(revealSection, {
     root: null,
-    threshold: 0.15,
+    threshold: 0.2,
   });
 
   allSections.forEach((section) => {
     sectionObserver.observe(section);
     if (section.id === "hero") return;
-    section.style.opacity = "0";
-    // section.style.transform = "translateY(0)";
+
+    section.classList.add(`${styles.disactive}`);
   });
 }
